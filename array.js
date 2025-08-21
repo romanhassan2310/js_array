02dcde3dcc8cb37d49ab0145aea5f48628829a74
Roman <romanhassan328@gmail.com>
2025-08-21 21:10:17 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,12 @@
+// const num1 = 12;
+// const num2 = 123;
+// const num3 = 125;
+// const num4 = 128;
+
+// const numbers = 12345632436;
+// (), {}, []
+
+const numbers = [12, 22, 54, 65];
+const friends = ['Abou', 'Kabil', 'Solim', 'Jolim'];
+const passed = [true, false, true, false];
+const mixArray = [12, 'Rohim', true, 45.564];
