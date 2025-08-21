4a4b304245384d17d19c0ddca4fc9d11f75a3973
Roman <romanhassan328@gmail.com>
2025-08-22 00:10:34 +0600

    added concat or combine connect array

---
+++ task5.js
@@ -0,0 +1,6 @@
+const fruits = ['Apple', 'Banana', 'Strawberry','Guava', 'Pepe','Watermelon' ];
+// console.log(fruits.splice(2,5));
+const friends = ['Salam', 'Kalam', 'Jolil', 'Kolil','Kodu'];
+// console.log(friends.join(fruits));
+
+console.log(friends.concat(fruits));
